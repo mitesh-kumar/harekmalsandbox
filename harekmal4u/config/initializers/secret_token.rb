8b592dd693e45dae29c89b21a0fe9fe039a24ed6
mitesh kumar <mtshkmr8@gmail.com>
2014-09-13 19:59:19 +0530

    Brand new project for onlineshop

---
+++ harekmal4u/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Harekmal4u::Application.config.secret_key_base = '49bff9dc5022dde9436fbf1c87a1d2cf951113da32034eefca82969c7f8782db6e714b628b7ff52bdb452cffa6e07b35658930f30ad9af48bf251276563c9359'
