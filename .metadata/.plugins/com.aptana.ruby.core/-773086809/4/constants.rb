3a764c9be66601dae54613579684e5f2f758ed2f
mitesh kumar <mtshkmr8@gmail.com>
2014-09-13 21:09:50 +0530

    yml file change for mysql adapter

---
+++ .metadata/.plugins/com.aptana.ruby.core/-773086809/4/constants.rb
@@ -0,0 +1,13 @@
+ARGV = []
+TRUE = true
+FALSE = false
+STDOUT = IO.new
+STDIN = IO.new
+STDERR = IO.new
+ENV = {}
+NIL = nil
+RUBY_PATCHLEVEL = 76
+RUBY_COPYRIGHT = 'ruby - Copyright (C) 1993-2014 Yukihiro Matsumoto'
+TOPLEVEL_BINDING = Binding.new
+RUBY_VERSION = '2.1.1'
+RUBY_PLATFORM = 'i686-linux'
